51286a2de0694f295804fb08cff2e6ffd233b061
Jing Wei Li <ljw9801055@gmail.com>
2020-05-01 12:21:24 -0400

    Revert "Initial Commit"
    This reverts commit 4de810633d553113f1f9d1cedd0b8fda1b8fb904.

--- DeviceIdSchema.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var DeviceIDSchema = new Schema({
-    id: String
-});
-
-module.exports = mongoose.model('DeviceID', DeviceIDSchema);
